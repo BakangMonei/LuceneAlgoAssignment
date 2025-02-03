88ef987b8cc0fccdeffc579e3da2a64538bc8381
BakangMonei <bakangmonei2@gmail.com>
2025-02-04 01:50:44 +0200

    first commit

---
+++ src/main/java/com/moneibakang/lucenealgoassignment/LuceneAlgoAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.moneibakang.lucenealgoassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuceneAlgoAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuceneAlgoAssignmentApplication.class, args);
+    }
+
+}
