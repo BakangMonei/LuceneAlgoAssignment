f89c57096706e373eb98557189e4e3b936d4746c
BakangMonei <bakangmonei2@gmail.com>
2025-02-06 20:00:00 +0200

    Hello

--- src/main/java/com/moneibakang/lucenealgoassignment/controllers/SearchController.java
+++ src/main/java/com/moneibakang/lucenealgoassignment/controllers/SearchController.java
@@ -1,4 +1,9 @@
 package com.moneibakang.lucenealgoassignment.controllers;
 
+/*
+ * @Author: Monei Bakang Mothuti
+ * @Time: 0200 hours
+ * @Date: 13/02/2025
+ */
 public class SearchController {
 }
---
+++ src/main/java/com/moneibakang/lucenealgoassignment/load/TextFileWords.java
@@ -0,0 +1,8 @@
+package com.moneibakang.lucenealgoassignment.load;
+/*
+ * @Author: Monei Bakang Mothuti
+ * @Time: 0200 hours
+ * @Date: 13/02/2025
+ */
+public class TextFileWords {
+}
