33ec807f97ee432c123210b09264224245534288
BakangMonei <bakangmonei2@gmail.com>
2025-02-13 00:06:00 +0200

    Hello

---
+++ src/main/java/com/moneibakang/lucenealgoassignment/service/lucene/DrillDownMustQuery.java
@@ -0,0 +1,9 @@
+package com.moneibakang.lucenealgoassignment.service.lucene;
+
+/*
+ * @Author: Monei Bakang Mothuti
+ * @Time: 0205 hours
+ * @Date: 13/02/2025
+ */
+public class DrillDownMustQuery {
+}
