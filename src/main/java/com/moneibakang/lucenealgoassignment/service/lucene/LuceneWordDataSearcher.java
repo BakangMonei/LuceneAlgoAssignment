40cd23145eeb74908fd3eacdb75d3560ed1b0c82
BakangMonei <bakangmonei2@gmail.com>
2025-02-13 02:07:04 +0200

    Awesome

---
+++ src/main/java/com/moneibakang/lucenealgoassignment/service/lucene/LuceneWordDataSearcher.java
@@ -0,0 +1,9 @@
+package com.moneibakang.lucenealgoassignment.service.lucene;
+
+/*
+ * @Author: Monei Bakang Mothuti
+ * @Time: 0206 hours
+ * @Date: 13/02/2025
+ */
+public class LuceneWordDataSearcher {
+}
